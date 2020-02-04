e0d0a33a675c45b37cfb9116d8fc93c2b95d8963
alfa9317 <josealfredotg@gmail.com>
2020-02-04 17:36:51 -0600

    Second server.js implementation

--- server.js
+++ server.js
@@ -1,27 +1,91 @@
-var cheerio = require("cheerio");
+var express = require("express");
+var logger = require("morgan");
+var mongoose = require("mongoose");
 var axios = require("axios");
+var cheerio = require("cheerio");
 
-console.log("\n***********************************\n" +
-            "Grabbing every thread name and link\n" +
-            "from screenrant" +
-            "\n***********************************\n");
+var db = require("./models");
 
-axios.get("https://screenrant.com/tv-news/").then(function(response) {
+var PORT = 3000;
 
-  var $ = cheerio.load(response.data);
+var app = express();
 
-  $("article h3").each(function(i, element) {
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
-    var result = {};
+app.use(express.static("public"));
 
-    result.title = $(this)
-    .children("a")
-    .text();
-    result.link = $(this)
-    .children("a")
-    .attr("href");
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scrapyquanchy";
+mongoose.connect(MONGODB_URI);
 
-    console.log(result);
+app.get("/scrape", function(req, res) {
 
-  });
+    axios.get("https://screenrant.com/tv-news/").then(function(response) {
+
+        var $ = cheerio.load(response.data);
+
+        $("article h3").each(function(i, element) {
+
+            var result = {};
+
+            result.title = $(this)
+            .children("a")
+            .text();
+            result.link = $(this)
+            .children("a")
+            .attr("href");
+
+            db.Article.create(result)
+            .then(function(dbArticle) {
+            console.log(dbArticle);
+            })
+            .catch(function(err) {
+            console.log(err);
+            });
+
+        });
+        res.send("Scrapyquanchy Complete");
+    });
 });
+
+app.get("/articles", function(req, res) {
+    db.Article.find({})
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  app.get("/articles/:id", function(req, res) {
+    db.Article.findOne({ _id: req.params.id })
+      .populate("note")
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+  
+  app.post("/articles/:id", function(req, res) {
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        res.json(err);
+      });
+  });
+
+  app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
+  
+
+
